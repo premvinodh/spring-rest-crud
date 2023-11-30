1b3c69efd0602441bd27562af98aebcc83bc5a91
premvinodh <premvinodh.pc@gmail.com>
2023-11-30 21:22:43 +0530

    Added h2 db properties to applicatoin.properties and updated LoadDatabase with comments on how to load data into db better

--- src/main/java/my/play/ground/springrestcrud/db/LoadDatabase.java
+++ src/main/java/my/play/ground/springrestcrud/db/LoadDatabase.java
@@ -9,6 +9,12 @@ import org.springframework.context.annotation.Configuration;
 import my.play.ground.springrestcrud.models.Employee;
 import my.play.ground.springrestcrud.repositories.EmployeeRepository;
 
+/**
+ * Instead of this class, you can also just create a 
+ * 			schema.sql and 
+ * 			data.sql and 
+ *  place those files in the resources folder - which is a much cleaner way to load the db. 
+ */
 @Configuration
 class LoadDatabase {
 
