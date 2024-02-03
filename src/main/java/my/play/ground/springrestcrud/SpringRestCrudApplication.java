280ca9a8405dc37525349097adfd59ac2be0d5ef
premvinodh <premvinodh.pc@gmail.com>
2024-02-03 23:55:23 +0530

    Added syso in main

--- src/main/java/my/play/ground/springrestcrud/SpringRestCrudApplication.java
+++ src/main/java/my/play/ground/springrestcrud/SpringRestCrudApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringRestCrudApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Entering main");
 		SpringApplication.run(SpringRestCrudApplication.class, args);
 	}
 }
