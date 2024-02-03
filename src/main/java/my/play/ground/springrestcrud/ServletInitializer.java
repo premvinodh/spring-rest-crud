d36e9dfc159d9ef647511a645716b79747e547ef
Bhanu Kodam <bhkodam@cisco.com>
2024-02-03 23:51:10 +0530

    test 1111111

--- src/main/java/my/play/ground/springrestcrud/ServletInitializer.java
+++ src/main/java/my/play/ground/springrestcrud/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		String s = "";
+		String s = "11111";
 		return application.sources(SpringRestCrudApplication.class);
 	}
 
